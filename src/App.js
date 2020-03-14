8bb0a8decbf44a22fb698b79b3b0c8a1b5d649c0
Miika Mehtälä <miikajom@Miika-MacBook-Air.local>
2020-03-14 11:38:19 +0200

    Changed to use separated topmenu component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
 import React from "react";
-import { Menu, Icon, Container } from "semantic-ui-react";
-
-const TopMenu = ({ props }) => (
-  <Menu>
-    <Container main>
-      <Menu.Item active={true}>
-        <Icon name="dashboard" />
-        Dashboard
-      </Menu.Item>
-      <Menu.Item>
-        <Icon name="warehouse" />
-        Warehouse
-      </Menu.Item>
-      <Menu.Item>
-        <Icon name="line graph" />
-        Reports
-      </Menu.Item>
-      <Menu.Item>
-        <Icon name="settings" />
-        Settings
-      </Menu.Item>
-    </Container>
-  </Menu>
-);
+import { Container } from "semantic-ui-react";
+import { TopMenu } from "./components/TopMenu";
 
 const App = () => (
-  <>
+  <React.Fragment>
     <TopMenu />
-    <Container main>Main content</Container>
-  </>
+    <Container>Main content</Container>
+  </React.Fragment>
 );
 
 export default App;
