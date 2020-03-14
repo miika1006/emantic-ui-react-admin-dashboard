993ae34d041339b8e090c5322b80f590b3367bc8
Miika Mehtälä <miikajom@Miika-MacBook-Air.local>
2020-03-14 11:46:47 +0200

    Created desktopsubmenu component

---
+++ src/components/DesktopSubMenu.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Menu } from "semantic-ui-react";
+
+export const DesktopSubMenu = () => {
+  return (
+    <Menu inverted color="blue" fluid style={{ borderRadius: "0" }}>
+      Submenu
+    </Menu>
+  );
+};
