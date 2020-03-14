d805cd343995d5072dfcaadc4f744bb46f3dfec5
Miika Mehtälä <miikajom@Miika-MacBook-Air.local>
2020-03-14 11:47:43 +0200

    Added DesktopSubMenu component

--- src/components/SubMenu.js
+++ src/components/SubMenu.js
@@ -1,7 +1,16 @@
 import React, { Fragment } from "react";
 import PropTypes from "prop-types";
+import { Responsive } from "semantic-ui-react";
+import { DesktopSubMenu } from "./DesktopSubMenu";
 
-export const SubMenu = ({ children }) => <Fragment>{children}</Fragment>;
+export const SubMenu = ({ children }) => (
+  <Fragment>
+    <Responsive minWidth={Responsive.onlyTablet.minWidth}>
+      <DesktopSubMenu>{children}</DesktopSubMenu>
+    </Responsive>
+  </Fragment>
+);
+
 SubMenu.propTypes = {
   children: PropTypes.node
 };
