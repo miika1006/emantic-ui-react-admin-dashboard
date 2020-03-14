74323599676eee0ddb20f57bdbde32a0f150eac6
Miika Mehtälä <miikajom@Miika-MacBook-Air.local>
2020-03-14 11:43:02 +0200

    imported and added SubMenu component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Container } from "semantic-ui-react";
 import { TopMenu } from "./components/TopMenu";
+import { SubMenu } from "./components/SubMenu";
 
 const App = () => (
   <React.Fragment>
     <TopMenu />
+    <SubMenu />
     <Container>Main content</Container>
   </React.Fragment>
 );
