b8239fb3e4165501fa0961c9a105dc6a231e7bdb
Miika Mehtälä <miikajom@Miika-MacBook-Air.local>
2020-03-15 13:04:03 +0200

    Removed container, alignment is now in Layout

--- src/components/DesktopMenu.js
+++ src/components/DesktopMenu.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Menu, Icon, Container } from "semantic-ui-react";
+import { Menu, Icon } from "semantic-ui-react";
 
 export const DesktopMenu = () => {
   const [activeItem, setActiveItem] = useState("dashboard");
@@ -10,39 +10,37 @@ export const DesktopMenu = () => {
   const borderRadiusZero = { borderRadius: "0" };
   return (
     <Menu inverted color="blue" fluid style={borderRadiusZero}>
-      <Container>
-        <Menu.Item
-          active={activeItem === "dashboard"}
-          onClick={toggleMenu("dashboard")}
-        >
-          <Icon name="dashboard" />
-          Dashboard
-        </Menu.Item>
-        <Menu.Item
-          active={activeItem === "warehouse"}
-          onClick={toggleMenu("warehouse")}
-        >
-          <Icon name="warehouse" />
-          Warehouse
-        </Menu.Item>
-        <Menu.Item
-          active={activeItem === "reports"}
-          onClick={toggleMenu("reports")}
-        >
-          <Icon name="line graph" />
-          Reports
-        </Menu.Item>
-        <Menu.Item
-          active={activeItem === "settings"}
-          onClick={toggleMenu("settings")}
-        >
-          <Icon name="settings" />
-          Settings
-        </Menu.Item>
-        <Menu.Item position="right" onClick={toggleMenu()}>
-          <Icon name="sign-out" />
-        </Menu.Item>
-      </Container>
+      <Menu.Item
+        active={activeItem === "dashboard"}
+        onClick={toggleMenu("dashboard")}
+      >
+        <Icon name="dashboard" />
+        Dashboard
+      </Menu.Item>
+      <Menu.Item
+        active={activeItem === "warehouse"}
+        onClick={toggleMenu("warehouse")}
+      >
+        <Icon name="warehouse" />
+        Warehouse
+      </Menu.Item>
+      <Menu.Item
+        active={activeItem === "reports"}
+        onClick={toggleMenu("reports")}
+      >
+        <Icon name="line graph" />
+        Reports
+      </Menu.Item>
+      <Menu.Item
+        active={activeItem === "settings"}
+        onClick={toggleMenu("settings")}
+      >
+        <Icon name="settings" />
+        Settings
+      </Menu.Item>
+      <Menu.Item position="right" onClick={toggleMenu()}>
+        <Icon name="sign-out" />
+      </Menu.Item>
     </Menu>
   );
 };
