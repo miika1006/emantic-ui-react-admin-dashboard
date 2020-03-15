d72256b22164c8ea29183d62d75da8327b930fa7
Miika Mehtälä <miikajom@Miika-MacBook-Air.local>
2020-03-15 14:39:20 +0200

    Added mobile submenu

--- src/components/SubMenu.js
+++ src/components/SubMenu.js
@@ -1,12 +1,16 @@
 import React, { Fragment } from "react";
 import { Responsive } from "semantic-ui-react";
 import { DesktopSubMenu } from "./DesktopSubMenu";
+import { MobileSubMenu } from "./MobileSubMenu";
 
 export const SubMenu = () => {
   return (
     <Fragment>
       <Responsive minWidth={Responsive.onlyTablet.minWidth}>
         <DesktopSubMenu />
+      </Responsive>
+      <Responsive maxWidth={Responsive.onlyMobile.maxWidth}>
+        <MobileSubMenu />
       </Responsive>
     </Fragment>
   );
