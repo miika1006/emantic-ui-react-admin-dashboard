59abddc62c81a2594ceb5070a91f4b6f8b574654
Miika Mehtälä <miikajom@Miika-MacBook-Air.local>
2020-03-15 13:04:28 +0200

    Created submenu items

--- src/components/DesktopSubMenu.js
+++ src/components/DesktopSubMenu.js
@@ -2,9 +2,13 @@ import React from "react";
 import { Menu } from "semantic-ui-react";
 
 export const DesktopSubMenu = () => {
+  const borderRadiusZero = { borderRadius: "0" };
   return (
-    <Menu inverted color="blue" fluid style={{ borderRadius: "0" }}>
-      Submenu
+    <Menu inverted color="blue" vertical fluid style={borderRadiusZero}>
+      <Menu.Item as="a">Submenu #1</Menu.Item>
+      <Menu.Item as="a">Submenu #2</Menu.Item>
+      <Menu.Item as="a">Submenu #3</Menu.Item>
+      <Menu.Item as="a">Submenu #4</Menu.Item>
     </Menu>
   );
 };
