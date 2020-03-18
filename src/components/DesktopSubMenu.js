c29086f0bb34fefa05f7c0b59ab9299b25813f8b
Miika Mehtälä <miikajom@Miika-MacBook-Air.local>
2020-03-18 18:26:09 +0200

    setup menuColor

--- src/components/DesktopSubMenu.js
+++ src/components/DesktopSubMenu.js
@@ -2,9 +2,10 @@ import React from "react";
 import { Menu } from "semantic-ui-react";
 
 export const DesktopSubMenu = () => {
+  const menuColor = "grey";
   const borderRadiusZero = { borderRadius: "0" };
   return (
-    <Menu inverted color="blue" vertical fluid style={borderRadiusZero}>
+    <Menu inverted color={menuColor} vertical fluid style={borderRadiusZero}>
       <Menu.Item as="a">Submenu #1</Menu.Item>
       <Menu.Item as="a">Submenu #2</Menu.Item>
       <Menu.Item as="a">Submenu #3</Menu.Item>
